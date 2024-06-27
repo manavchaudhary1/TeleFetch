7ff6a52c6c2a292b001e7a4dcf6b24447dc9f916
manavchaudhary1 <manavchaudhary041@gmail.com>
2024-06-27 15:35:41 +0530

    Theme change to Lumo.Dark in vaadin

--- src/main/java/dev/manav/telefetch/TeleFetchApplication.java
+++ src/main/java/dev/manav/telefetch/TeleFetchApplication.java
@@ -1,10 +1,15 @@
 package dev.manav.telefetch;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
+import com.vaadin.flow.theme.Theme;
+import com.vaadin.flow.theme.lumo.Lumo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TeleFetchApplication {
+@Theme(variant = Lumo.DARK)
+public class TeleFetchApplication implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(TeleFetchApplication.class, args);
